2038e844105d1186eb855e96123ef3a4f2ebe67b
ASIC-Design <71587447+ASIC-Design@users.noreply.github.com>
2023-12-05 17:58:49 +0800

    Update kore_opfsm.v

--- kore_opfsm.v
+++ kore_opfsm.v
@@ -5,22 +5,20 @@ module kore_opfsm (
 
   input    clk    ,
   input    rst_n    ,
-  input    IR_code  , [31:0] pcdata_in ,    
+  input    [31:0] pcdata_in ,    //from  ir reg 
 
-  input    din_hs  ,   //data in hand shake 
 
   input    eop,    //from func fsm
 
-
-  output    opcode      ,
-  output    pcdata_rs0  ,
-  output    pcdata_rs1  ,
-  output    pcdata_rd   ,  
-  output    pcdata_bc    ,
-
-  output    opflag    
+  output  reg [7:0]     pc_sel  ,   //is pcdata in [32:25] to choose IR reg  
   
+  output reg [6:0]   opcode      ,  
+  output reg [4:0]   pcdata_rs0  ,  
+  output reg [4:0]   pcdata_rs1  ,  
+  output reg [4:0]   pcdata_rd   ,  
+  output reg [2:0]   pcdata_bc   ,  
 
+  output reg         opflag      
 
 );
 
@@ -34,10 +32,7 @@ localparam   S4   =  8'H4;   //S0
 
   reg  [7:0] cs ;
   reg  [7:0]  ns ;
-  
-  wire  [31:0] IR_code;
 
-  assign  IR_code = IR_code  ;  //from IR reg
 
   always@(posedge clk or negedge rst_n ) begin
     if(!rst_n)  begin
@@ -92,27 +87,58 @@ localparam   S4   =  8'H4;   //S0
   always@(posedge clk or negedge rst_n) begin
     case(ns)
       IDLE: begin
-        opcode[6:0] <= pcdata_in [6:0] ;
-        opflag    <= 1'b0   ;
-        din_vld   <= 1'b1   ;   //wait for hand shake
-        dout      <= 32'b0  ;
-        dout_rdy  <= 1'b0   ;   //not rdy out
+        pc_sel[7:0]      <= pcdata_in[32:25]  ;   //  func8
+        pcdata_rs0[4:0]  <= pcdata_in[24:20]  ;   //
+        pcdata_rs1[4:0]  <= pcdata_in[19:15]  ;   //
+        pcdata_bc [2:0]  <= pcdata_in[14:12] ;  // 
+        pcdata_rd [4:0]  <= pcdata_in[11:7]  ;   //
+        opcode[6:0]      <= pcdata_in [6:0] ;
+        opflag           <= 1'b0   ;
       end
       S1:  begin
-          opflag  <= 1'b1  ;  //
+        pc_sel[7:0]      <= pcdata_in[32:25]  ;   //  func8
+        pcdata_rs0[4:0]  <= pcdata_in[24:20]  ;   //
+        pcdata_rs1[4:0]  <= pcdata_in[19:15]  ;   //
+        pcdata_bc [2:0]  <= pcdata_in[14:12] ;  // 
+        pcdata_rd [4:0]  <= pcdata_in[11:7]  ;   //
+        opcode[6:0]      <= pcdata_in [6:0] ;        
+        opflag           <= 1'b1  ;  //
       end
       S2:  begin
-          opflag <= 1'b0;   //wait to fan out
-          
+        pc_sel[7:0]      <= pcdata_in[32:25]  ;   //  func8
+        pcdata_rs0[4:0]  <= pcdata_in[24:20]  ;   //
+        pcdata_rs1[4:0]  <= pcdata_in[19:15]  ;   //
+        pcdata_bc [2:0]  <= pcdata_in[14:12] ;  // 
+        pcdata_rd [4:0]  <= pcdata_in[11:7]  ;   //
+        opcode[6:0]      <= pcdata_in [6:0] ;    
+        opflag <= 1'b0;   //wait to fan out          
       end
       S3:  begin
+        pc_sel[7:0]      <= pcdata_in[32:25]  ;   //  func8
+        pcdata_rs0[4:0]  <= pcdata_in[24:20]  ;   //
+        pcdata_rs1[4:0]  <= pcdata_in[19:15]  ;   //
+        pcdata_bc [2:0]  <= pcdata_in[14:12] ;  // 
+        pcdata_rd [4:0]  <= pcdata_in[11:7]  ;   //
+        opcode[6:0]      <= pcdata_in [6:0] ;    
           opflag <= 1'b1;   //
       end
       S4:begin
-         opflag <= 1'b0;   //wait to fan out
+        pc_sel[7:0]      <= pcdata_in[32:25]  ;   //  func8
+        pcdata_rs0[4:0]  <= pcdata_in[24:20]  ;   //
+        pcdata_rs1[4:0]  <= pcdata_in[19:15]  ;   //
+        pcdata_bc [2:0]  <= pcdata_in[14:12] ;  // 
+        pcdata_rd [4:0]  <= pcdata_in[11:7]  ;   //
+        opcode[6:0]      <= pcdata_in [6:0] ;    
+        opflag <= 1'b0;   //wait to fan out
       end
       default:begin
-         opflag <= 1'b0;   //wait to fan out
+        pc_sel[7:0]      <= pcdata_in[32:25]  ;   //  func8
+        pcdata_rs0[4:0]  <= pcdata_in[24:20]  ;   //
+        pcdata_rs1[4:0]  <= pcdata_in[19:15]  ;   //
+        pcdata_bc [2:0]  <= pcdata_in[14:12] ;  // 
+        pcdata_rd [4:0]  <= pcdata_in[11:7]  ;   //
+        opcode[6:0]      <= pcdata_in [6:0] ;    
+        opflag <= 1'b0;   //wait to fan out
       end
     endcase
 
